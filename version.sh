97ffdfc02cc276109857440da72b6bff920f0159
Alexander F. Rødseth <rodseth@gmail.com>
2021-12-10 15:05:53 +0100

    Add version script

---
+++ version.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+#
+# Self-modifying script that updates the version numbers
+#
+
+# The current version goes here, as the default value
+VERSION=${1:-'1.5.0'}
+
+if [ -z "$1" ]; then
+  echo "The current version is $VERSION, pass the new version as the first argument if you wish to change it"
+  exit 0
+fi
+
+echo "Setting the version to $VERSION"
+
+# Set the version in various files
+setconf README.md '* Version' $VERSION
+setconf version.go "const VersionString" "\"png2svg "$VERSION"\""
+
+# Update the version in this script
+sed -i "s/[[:digit:]]*\.[[:digit:]]*\.[[:digit:]]*/$VERSION/g" "$0"
