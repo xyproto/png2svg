4aa30bcec22a6759a2d868c679544047cd1a4621
Alexander F. Rødseth <rodseth@gmail.com>
2021-12-16 10:29:59 +0100

    Use a Config struct

--- cmd/png2svg/main.go
+++ cmd/png2svg/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/xyproto/png2svg"
@@ -15,58 +17,54 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
-func main() {
-	var (
-		outputFilename        string
-		colorPink             bool
-		singlePixelRectangles bool
-		verbose               bool
-		version               bool
-		quantize              bool
-		limit                 bool
-		colorOptimize         bool
-	)
+type Config struct {
+	inputFilename         string
+	outputFilename        string
+	colorOptimize         bool
+	colorPink             bool
+	limit                 bool
+	quantize              bool
+	singlePixelRectangles bool
+	verbose               bool
+	version               bool
+}
 
-	// TODO: Use a proper package for flag handling
-	flag.StringVar(&outputFilename, "o", "-", "SVG output filename")
-	flag.BoolVar(&singlePixelRectangles, "p", false, "use only single pixel rectangles")
-	flag.BoolVar(&colorPink, "c", false, "color expanded rectangles pink")
-	flag.BoolVar(&verbose, "v", false, "verbose")
-	flag.BoolVar(&version, "V", false, "version")
-	flag.BoolVar(&limit, "l", false, "limit colors to a maximum of 4096 (#abcdef -> #ace)")
-	flag.BoolVar(&quantize, "q", false, "deprecated (same as -l)")
-	flag.BoolVar(&colorOptimize, "z", false, "deprecated (same as -l)")
+// NewConfigFromFlags returns a Config struct, a quit message (for -v) and/or an error
+func NewConfigFromFlags() (*Config, string, error) {
+	var c Config
 
-	limit = limit || quantize || colorOptimize
+	flag.StringVar(&c.outputFilename, "o", "-", "SVG output filename")
+	flag.BoolVar(&c.singlePixelRectangles, "p", false, "use only single pixel rectangles")
+	flag.BoolVar(&c.colorPink, "c", false, "color expanded rectangles pink")
+	flag.BoolVar(&c.verbose, "v", false, "verbose")
+	flag.BoolVar(&c.version, "V", false, "version")
+	flag.BoolVar(&c.limit, "l", false, "limit colors to a maximum of 4096 (#abcdef -> #ace)")
+	flag.BoolVar(&c.quantize, "q", false, "deprecated (same as -l)")
+	flag.BoolVar(&c.colorOptimize, "z", false, "deprecated (same as -l)")
 
 	flag.Parse()
 
-	if version {
-		fmt.Println(png2svg.VersionString)
-		return
+	if c.version {
+		return nil, png2svg.VersionString, nil
 	}
 
-	if colorPink {
-		singlePixelRectangles = false
+	c.limit = c.limit || c.quantize || c.colorOptimize
+
+	if c.colorPink {
+		c.singlePixelRectangles = false
 	}
 
 	args := flag.Args()
 	if len(args) == 0 {
-		fmt.Fprintf(os.Stderr, "An input PNG filename is required.\n")
-		os.Exit(1)
+		return nil, "", errors.New("an input PNG filename is required")
+
 	}
+	c.inputFilename = args[0]
+	return &c, "", nil
+}
 
-	inputFilename := args[0]
-
-	img, err := png2svg.ReadPNG(inputFilename, verbose)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
-		os.Exit(1)
-	}
-
+func Run() error {
 	var (
-		height       = img.Bounds().Max.Y - img.Bounds().Min.Y
-		pi           = png2svg.NewPixelImage(img, verbose)
 		box          *png2svg.Box
 		x, y         int
 		expanded     bool
@@ -75,9 +73,25 @@ func main() {
 		done         bool
 	)
 
-	pi.SetColorOptimize(limit)
+	c, quitMessage, err := NewConfigFromFlags()
+	if err != nil {
+		return err
+	} else if quitMessage != "" {
+		fmt.Println(quitMessage)
+		return nil
+	}
 
-	if verbose {
+	img, err := png2svg.ReadPNG(c.inputFilename, c.verbose)
+	if err != nil {
+		return err
+	}
+
+	height := img.Bounds().Max.Y - img.Bounds().Min.Y
+
+	pi := png2svg.NewPixelImage(img, c.verbose)
+	pi.SetColorOptimize(c.limit)
+
+	if c.verbose {
 		fmt.Print("Placing rectangles... 0%")
 	}
 
@@ -85,12 +99,12 @@ func main() {
 	lastPercentage := 0
 
 	// Cover pixels by creating expanding rectangles, as long as there are uncovered pixels
-	for !singlePixelRectangles && !done {
+	for !c.singlePixelRectangles && !done {
 
 		// Select the first uncovered pixel, searching from the given coordinate
 		x, y = pi.FirstUncovered(lastx, lasty)
 
-		if verbose && y != lastLine {
+		if c.verbose && y != lastLine {
 			lastPercentage = percentage
 			percentage = int((float64(y) / float64(height)) * 100.0)
 			png2svg.Erase(len(fmt.Sprintf("%d%%", lastPercentage)))
@@ -110,26 +124,29 @@ func main() {
 		//expanded = pi.ExpandRandom(box)
 
 		// Use the expanded box. Color pink if it is > 1x1, and colorPink is true
-		pi.CoverBox(box, expanded && colorPink, limit)
+		pi.CoverBox(box, expanded && c.colorPink, c.limit)
 
 		// Check if we are done, searching from the current x,y
 		done = pi.Done(x, y)
 	}
 
-	if verbose {
+	if c.verbose {
 		png2svg.Erase(len(fmt.Sprintf("%d%%", lastPercentage)))
 		fmt.Println("100%")
 	}
 
-	if singlePixelRectangles {
+	if c.singlePixelRectangles {
 		// Cover all remaining pixels with rectangles of size 1x1
 		pi.CoverAllPixels()
 	}
 
 	// Write the SVG image to outputFilename
-	err = pi.WriteSVG(outputFilename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+	return pi.WriteSVG(c.outputFilename)
+}
+
+func main() {
+	if err := Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", strings.Title(err.Error()))
 		os.Exit(1)
 	}
 }
