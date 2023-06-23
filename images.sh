7929f3e0dad5cc5fd79989bbffdf895e0f29f48f
Alexander F. Rødseth <rodseth@gmail.com>
2023-06-23 14:40:39 +0200

    Update images

--- images.sh
+++ images.sh
@@ -2,6 +2,12 @@
 
 (cd cmd/png2svg; go build -mod=vendor -v)
 
+for x in 128 96 64 32 16 8 6 4 2; do
+  cmd/png2svg/png2svg -n ${x} -v -l -o img/rainforest_${x}c.svg img/rainforest.png
+  echo svgo
+  svgo --multipass img/rainforest_${x}c.svg -o img/rainforest_${x}c_opt.svg
+done
+
 cmd/png2svg/png2svg -v -l -o img/rainforest4096.svg img/rainforest.png
 echo svgo
 svgo --multipass img/rainforest4096.svg -o img/rainforest_opt.svg
@@ -9,9 +15,3 @@ svgo --multipass img/rainforest4096.svg -o img/rainforest_opt.svg
 cmd/png2svg/png2svg -v -l -o img/spaceships4096.svg img/spaceships.png
 echo svgo
 svgo --multipass img/spaceships4096.svg -o img/spaceships_opt.svg
-
-for x in 96 64 32 16 8 4; do
-  cmd/png2svg/png2svg -n${x} -v -l -o img/rainforest_${x}c.svg img/rainforest.png
-  echo svgo
-  svgo --multipass img/rainforest_${x}c.svg -o img/rainforest_${x}c_opt.svg
-done
